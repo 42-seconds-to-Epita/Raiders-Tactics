a7d83901536c99f7f69b9acc966cc23301fd4dce
Popop12 <kevin.lubert@epita.fr>
2023-02-28 11:59:11 +0100

    [Units Spawn] Fix

--- Assets/Scripts/UnitSpawner.cs
+++ Assets/Scripts/UnitSpawner.cs
@@ -1,6 +1,8 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Mirror;
+using QuickStart.Utils;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
@@ -20,22 +22,34 @@ public class UnitSpawner : NetworkBehaviour, IPointerClickHandler
     }
     
     #endregion
-    
-    
-    
+
+    public void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Vector3 pos = PlayerInteractUtils.GetMouseWorldPosition();
+            CmdSpawnUnit();
+        }
+    }
+
+
     #region Client
     
     public void OnPointerClick(PointerEventData eventData)
     {
+        Debug.Log("1");
         if (eventData.button != PointerEventData.InputButton.Left)
         {
             return;
         }
+        Debug.Log("2");
 
         if (!hasAuthority)
         {
             return;
         }
+        
+        Debug.Log("3");
         
         CmdSpawnUnit();
 
